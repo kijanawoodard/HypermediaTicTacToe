3401aec0452bc83671754586d785762fc60be1e2
Kijana Woodard <github@wyldeye.com>
2017-01-20 23:58:07 -0600

    add play accepted event

--- src/TicTacToeEngine/Program.fs
+++ src/TicTacToeEngine/Program.fs
@@ -9,22 +9,36 @@ type Column = Left | Center | Right
 type Mark = Mark of Player | Empty
 type Position = Position of Row * Column
 
+type Game = { 
+    Grid: Map<Position, Mark>
+    Next: Player
+}
+
+type PlayAccepted = {
+    Position: Position 
+    Player: Player
+}
 let createGrid = [
     for r in FSharpType.GetUnionCases typeof<Row> do
     for c in FSharpType.GetUnionCases typeof<Column> do
     let row = FSharpValue.MakeUnion(r, [| |]) :?> Row
     let column = FSharpValue.MakeUnion(c, [| |]) :?> Column
     yield Position (row, column), Empty
-] 
+]
+
+let acceptPlay game event =
+    let mark = Mark event.Player
+    { game with Grid = Map.add event.Position mark game.Grid }
 
 let markGrid grid (position:Position) player =
+    let mark = Mark player
     match Map.find position grid with
      | Mark _ -> grid
-     | Empty -> Map.add position player grid
+     | Empty -> Map.add position mark grid
 
 let start = createGrid |> Map.ofSeq
-let playX = markGrid start (Position (Top, Left)) (Mark PlayerX)
-let playO = markGrid playX (Position (Top, Left)) (Mark PlayerO)
+let playX = markGrid start (Position (Top, Left)) PlayerX
+let playO = markGrid playX (Position (Top, Left)) PlayerO
 
 [<EntryPoint>]
 let main argv = 
