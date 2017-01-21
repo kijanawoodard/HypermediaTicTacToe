a78a91d80aa8161509ddc28b683c2b90eee4e525
Kijana Woodard <github@wyldeye.com>
2017-01-20 22:33:54 -0600

    add Mark and Position

--- src/TicTacToeEngine/Program.fs
+++ src/TicTacToeEngine/Program.fs
@@ -6,28 +6,25 @@ open Microsoft.FSharp.Reflection
 type Player = PlayerX | PlayerO
 type Row = Top | Middle | Bottom
 type Column = Left | Center | Right
-type Position = Row * Column
-
-let createCell position (player: Player option) =
-    (position, player)
+type Mark = Mark of Player | Empty
+type Position = Position of Row * Column
 
 let createGrid = [
     for r in FSharpType.GetUnionCases typeof<Row> do
     for c in FSharpType.GetUnionCases typeof<Column> do
     let row = FSharpValue.MakeUnion(r, [| |]) :?> Row
     let column = FSharpValue.MakeUnion(c, [| |]) :?> Column
-    yield createCell (row, column) None
+    yield Position (row, column), Empty
 ] 
-
 let grid = createGrid |> Map.ofSeq
-let x = grid.Add((Top, Left), Some PlayerO)
-let markGrid grid position player =
-    match Map.find position grid with
-     | Some marked -> grid
-     | None -> Map.add position player grid
 
-let playX = markGrid grid (Top, Left) (Some PlayerX)
-let playO = markGrid playX (Top, Left) (Some PlayerO)
+let markGrid grid (position:Position) (player: Player) =
+    match Map.find position grid with
+     | Mark _ -> grid
+     | Empty -> Map.add position (Mark player) grid
+
+let playX = markGrid grid (Position (Top, Left)) PlayerX
+let playO = markGrid playX (Position (Top, Left)) PlayerO
 
 [<EntryPoint>]
 let main argv = 
